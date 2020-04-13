ae498e655f5404fe37e7f2f172d39eaa6aecf424
Austin <aturner9967@gmail.com>
2020-04-13 12:10:01 -0600

    Added test for ignores

--- test/test_namecase.rb
+++ test/test_namecase.rb
@@ -1,31 +1,35 @@
-# -*- coding: utf-8 -*-
-#
+# frozen_string_literal: true
+
 require 'namecase'
 require 'minitest/autorun'
 
 class TestNameCase < Minitest::Test
   def setup
     @proper_names = [
-      "Keith",            "Leigh-Williams",       "McCarthy",
-      "O'Callaghan",      "St. John",             "von Streit",
-      "van Dyke",         "Van",                  "ap Llwyd Dafydd",
-      "al Fahd",          "Al",
-      "el Grecco",
-      "ben Gurion",       "Ben",
-      "da Vinci",
-      "di Caprio",        "du Pont",              "de Legate",
-      "del Crond",        "der Sind",             "van der Post",
-      "von Trapp",        "la Poisson",           "le Figaro",
-      "Mack Knife",       "Dougal MacDonald",
-      "Ruiz y Picasso",   "Dato e Iradier",       "Mas i Gavarró",
+      'Keith',            'Leigh-Williams',       'McCarthy',
+      "O'Callaghan",      'St. John',             'von Streit',
+      'van Dyke',         'Van',                  'ap Llwyd Dafydd',
+      'al Fahd',          'Al',
+      'el Grecco',
+      'ben Gurion', 'Ben',
+      'da Vinci',
+      'di Caprio',        'du Pont',              'de Legate',
+      'del Crond',        'der Sind',             'van der Post',
+      'von Trapp',        'la Poisson',           'le Figaro',
+      'Mack Knife',       'Dougal MacDonald',
+      'Ruiz y Picasso',   'Dato e Iradier',       'Mas i Gavarró',
       # Mac exceptions
-      "Machin",           "Machlin",              "Machar",
-      "Mackle",           "Macklin",              "Mackie",
-      "Macquarie",        "Machado",              "Macevicius",
-      "Maciulis",         "Macias",               "MacMurdo",
+      'Machin',           'Machlin',              'Machar',
+      'Mackle',           'Macklin',              'Mackie',
+      'Macquarie',        'Machado',              'Macevicius',
+      'Maciulis',         'Macias',               'MacMurdo',
       # Roman numerals
-      "Henry VIII",       "Louis III",            "Louis XIV",
-      "Charles II",       "Fred XLIX",            "Yusof bin Ishak",
+      'Henry VIII',       'Louis III',            'Louis XIV',
+      'Charles II',       'Fred XLIX',            'Yusof bin Ishak'
+    ]
+
+    @exception_cases = [
+      { exception: { hebrew: true }, test_case: 'Ben Test Name' }
     ]
   end
 
@@ -50,5 +54,13 @@ class TestNameCase < Minitest::Test
     proper_cased = 'Iñtërnâtiônàlizætiøn'
     nc_name = NameCase(proper_cased.downcase)
     assert_equal(proper_cased, nc_name)
+  end
+
+  def test_namecase_ignore
+    @exception_cases.each do |name_hash|
+      assert_equal(name_hash[:test_case], NameCase(name_hash[:test_case].downcase, ignore: name_hash[:exception]))
+      assert_equal(name_hash[:test_case], NameCase(name_hash[:test_case].upcase, ignore: name_hash[:exception]))
+      assert_equal(name_hash[:test_case], NameCase(name_hash[:test_case], ignore: name_hash[:exception]))
+    end
   end
 end
