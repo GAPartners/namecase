a3c430561524f8364fed73b856f7f78d10a8606e
Austin <aturner9967@gmail.com>
2020-04-13 11:28:49 -0600

    Added ignore field for hebrew

--- lib/namecase.rb
+++ lib/namecase.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module NameCase
   VERSION = '2.0.0'
 
@@ -11,12 +13,12 @@ module NameCase
     NameCase.nc! self, options
   end
 
-  def self.nc! str, options = {}
+  def self.nc!(str, options = {})
     str.replace NameCase.nc(str, options)
   end
 
-  def self.nc str, options = {}
-    options = { :lazy => true, :irish => true, :spanish => true }.merge options
+  def self.nc(str, options = {})
+    options = { lazy: true, irish: true, spanish: true, ignore: {} }.merge options
 
     # Skip if string is mixed case
     if options[:lazy]
@@ -27,11 +29,11 @@ module NameCase
     end
 
     localstring = str.downcase
-    localstring.gsub!(/\b\w/) { |first| first.upcase }
-    localstring.gsub!(/\'\w\b/) { |c| c.downcase } # Lowercase 's
+    localstring.gsub!(/\b\w/, &:upcase)
+    localstring.gsub!(/\'\w\b/, &:downcase) # Lowercase 's
 
     if options[:irish]
-      if localstring =~ /\bMac[A-Za-z]{2,}[^aciozj]\b/ or localstring =~ /\bMc/
+      if localstring =~ /\bMac[A-Za-z]{2,}[^aciozj]\b/ || localstring =~ /\bMc/
         match = localstring.match(/\b(Ma?c)([A-Za-z]+)/)
         localstring.gsub!(/\bMa?c[A-Za-z]+/) { match[1] + match[2].capitalize }
 
@@ -50,32 +52,33 @@ module NameCase
         localstring.gsub!(/\bMacKmin/, 'Mackmin')
         localstring.gsub!(/\bMacQuarie/, 'Macquarie')
       end
-      localstring.gsub!('Macmurdo','MacMurdo')
+      localstring.gsub!('Macmurdo', 'MacMurdo')
     end
 
     # Fixes for "son (daughter) of" etc
-    localstring.gsub!(/\bAl(?=\s+\w)/, 'al')  # al Arabic or forename Al.
-    localstring.gsub!(/\b(Bin|Binti|Binte)\b/,'bin')  # bin, binti, binte Arabic
-    localstring.gsub!(/\bAp\b/, 'ap')         # ap Welsh.
-    localstring.gsub!(/\bBen(?=\s+\w)/,'ben') # ben Hebrew or forename Ben.
-    localstring.gsub!(/\bDell([ae])\b/,'dell\1')  # della and delle Italian.
-    localstring.gsub!(/\bD([aeiou])\b/,'d\1')   # da, de, di Italian; du French; do Brasil
-    localstring.gsub!(/\bD([ao]s)\b/,'d\1')   # das, dos Brasileiros
-    localstring.gsub!(/\bDe([lr])\b/,'de\1')   # del Italian; der Dutch/Flemish.
-    localstring.gsub!(/\bEl\b/,'el')   # el Greek or El Spanish.
-    localstring.gsub!(/\bLa\b/,'la')   # la French or La Spanish.
-    localstring.gsub!(/\bL([eo])\b/,'l\1')      # lo Italian; le French.
-    localstring.gsub!(/\bVan(?=\s+\w)/,'van')  # van German or forename Van.
-    localstring.gsub!(/\bVon\b/,'von')  # von Dutch/Flemish
+    localstring.gsub!(/\bAl(?=\s+\w)/, 'al') # al Arabic or forename Al.
+    localstring.gsub!(/\b(Bin|Binti|Binte)\b/, 'bin') # bin, binti, binte Arabic
+    localstring.gsub!(/\bAp\b/, 'ap') # ap Welsh.
+    # ben Hebrew or forename Ben.
+    localstring.gsub!(/\bBen(?=\s+\w)/, 'ben') unless ignore[:hebrew]
+    localstring.gsub!(/\bDell([ae])\b/, 'dell\1') # della and delle Italian.
+    localstring.gsub!(/\bD([aeiou])\b/, 'd\1') # da, de, di Italian; du French; do Brasil
+    localstring.gsub!(/\bD([ao]s)\b/, 'd\1') # das, dos Brasileiros
+    localstring.gsub!(/\bDe([lr])\b/, 'de\1') # del Italian; der Dutch/Flemish.
+    localstring.gsub!(/\bEl\b/, 'el')   # el Greek or El Spanish.
+    localstring.gsub!(/\bLa\b/, 'la')   # la French or La Spanish.
+    localstring.gsub!(/\bL([eo])\b/, 'l\1') # lo Italian; le French.
+    localstring.gsub!(/\bVan(?=\s+\w)/, 'van') # van German or forename Van.
+    localstring.gsub!(/\bVon\b/, 'von') # von Dutch/Flemish
 
     # Fix roman numeral names
     localstring.gsub!(
       / \b ( (?: [Xx]{1,3} | [Xx][Ll]   | [Ll][Xx]{0,3} )?
-             (?: [Ii]{1,3} | [Ii][VvXx] | [Vv][Ii]{0,3} )? ) \b /x
-    ) { |m| m.upcase }
+             (?: [Ii]{1,3} | [Ii][VvXx] | [Vv][Ii]{0,3} )? ) \b /x, &:upcase
+    )
 
     if options[:spanish]
-      ["Y", "E", "I"].each do |conjunction|
+      %w[Y E I].each do |conjunction|
         localstring.gsub!(/\b#{conjunction}\b/, conjunction.downcase)
       end
     end
